f281063fb6b76bdceefcbb6f946b37a0cb9df46f
Sydney Dickson <sydneyrachelledickson@gmail.com>
2023-04-12 09:58:02 -0500

    kek

--- src/manager.js
+++ src/manager.js
@@ -21,31 +21,33 @@ export const getResponse = async (input) => {
     return data.choices[0];
     };
 
-    export const voiceTranslate = async (input) => {
-      const options = {
-        method: 'POST',
-        headers: {
-          accept: 'application/json',
-          'uberduck-id': 'anond98e3de5-8b78-4706-98ae-e24058aaf97cymous',
-          'content-type': 'application/json',
-          authorization: 'Basic ' + btoa(API_KEY + ':' + API_SECRET)
-        },
-        body: JSON.stringify({voicemodel_uuid: "d98e3de5-8b78-4706-98ae-e24058aaf97c", pace: 1, speech: `${input}`})
-      };
-      
-      return fetch('https://proxy.cors.sh/https://api.uberduck.ai/speak-synchronous', options)
-      .then(response => response.arrayBuffer())
-      .then(arrayBuffer => {
-        const AudioContext = window.AudioContext || window.webkitAudioContext;
-        const audioContext = new AudioContext();
-        audioContext.decodeAudioData(arrayBuffer, buffer => {
-          const source = audioContext.createBufferSource();
-          source.buffer = buffer;
-          source.connect(audioContext.destination);
-          source.start(0);
-        });
-      })
-      .catch(err => console.error(err));
-        
-        
-        }
+export const voiceTranslate = async (input) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'x-cors-api-key': 'cors_key',
+      accept: 'application/json',
+      'uberduck-id': 'anond98e3de5-8b78-4706-98ae-e24058aaf97cymous',
+      'content-type': 'application/json',
+      authorization: 'Basic' + btoa(API_KEY + ':' + API_SECRET)
+    },
+    body: JSON.stringify({voicemodel_uuid: "d98e3de5-8b78-4706-98ae-e24058aaf97c", pace: 1, speech: `${input}`})
+  };
+  
+  return fetch('https://proxy.cors.sh/https://api.uberduck.ai/speak-synchronous', options)
+  .then(response => response.arrayBuffer())
+  .then(arrayBuffer => {
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    const audioContext = new AudioContext();
+    audioContext.decodeAudioData(arrayBuffer, buffer => {
+      const source = audioContext.createBufferSource();
+      source.buffer = buffer;
+      source.connect(audioContext.destination);
+      source.start(0);
+    });
+  })
+  .catch(err => console.error(err));
+    
+    
+    }
+
